bfa10619027cff85ee3de6151ebcc13e38b3d049
Tommaso Resti <tommaso.resti@lastminute.com>
2016-10-05 15:45:44 +0200

    Operations

---
+++ TDD_Course/Operation.h
@@ -0,0 +1,12 @@
+//
+// Created by Tommaso Resti on 05/10/16.
+// Copyright (c) 2016 Tommaso Resti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Command;
+
+@protocol Operation <NSObject>
+-(void)run:(Command *)command;
+@end
--- TDD_Course/PointOfSale.m
+++ TDD_Course/PointOfSale.m
@@ -10,36 +10,53 @@
 #import "Scanner.h"
 #import "Cart.h"
 #import "BarcodeScannedCommand.h"
+#import "Operation.h"
 
+@interface FinishOperation: NSObject <Operation>
+@property (weak) id<Display> display;
+@property (weak) Cart *cart;
 
-@interface PointOfSale ()
-@property(nonatomic, strong) id catalog;
-@property(nonatomic, strong) id display;
-@property(nonatomic, strong) Cart *cart;
++ (id)operationWithCart:(Cart *)cart andDisplay:(id)display;
 @end
 
-@implementation PointOfSale
-+ (PointOfSale *)pointOfSaleWithCatalog:(id)catalog andDisplay:(id)display {
-  PointOfSale *sale = [[PointOfSale alloc] init];
-  sale.catalog = catalog;
-  sale.display = display;
-  sale.cart = [Cart cartWithPrices:@[]];
-  return sale;
+@implementation FinishOperation
+- (void)run:(Command *)command {
+  [self.display displayTotal:self.cart.total];
 }
 
-+ (PointOfSale *)pointOfSaleWithCatalog:(id <Catalog>)catalog andDisplay:(id <Display>)display andCart:(Cart *)cart {
-  PointOfSale *sale = [[PointOfSale alloc] init];
-  sale.catalog = catalog;
-  sale.display = display;
-  sale.cart = cart;
-  return sale;
++ (id)operationWithCart:(Cart *)cart andDisplay:(id)display {
+  FinishOperation *operation = [FinishOperation new];
+  operation.cart = cart;
+  operation.display = display;
+  return operation;
+}
+@end
+
+@interface BarcodeScannedOperation : NSObject <Operation>
+@property (weak) id<Display> display;
+@property (weak) id<Catalog> catalog;
+@property (weak) Cart *cart;
+
++ (id)operationWithCatalog:(id)catalog cart:(Cart *)cart andDisplay:(id)display;
+@end
+
+@implementation BarcodeScannedOperation
+- (void)run:(Command *)command {
+  [self onBarcodeScanned:(BarcodeScannedCommand *) command];
 }
 
-- (void)executeScannerCommand:(BarcodeScannedCommand *)command {
++ (id)operationWithCatalog:(id)catalog cart:(Cart *)cart andDisplay:(id)display {
+  BarcodeScannedOperation *operation = [BarcodeScannedOperation new];
+  operation.display = display;
+  operation.cart = cart;
+  operation.catalog = catalog;
+  return operation;
+}
+
+- (void)onBarcodeScanned:(BarcodeScannedCommand *)command {
   Price *price = [self.catalog findPrice:command.barcode];
-  if(price) {
+  if(price)
     [self priceFound:price];
-  }
   else
     [self.display displayPriceNotFound];
 }
@@ -49,17 +66,47 @@
   [self.display displayPriceFound:price];
 }
 
--(void)executeFinishCommand:(FinishCommand *)command {
-  [self.display displayTotal:self.cart.total];
+@end
+
+
+@interface PointOfSale ()
+@property(nonatomic, strong) id catalog;
+@property(nonatomic, strong) id display;
+@property(nonatomic, strong) Cart *cart;
+@property(nonatomic, strong) NSDictionary *operations;
+@end
+
+@implementation PointOfSale
++ (PointOfSale *)pointOfSaleWithCatalog:(id)catalog andDisplay:(id)display {
+  PointOfSale *sale = [[PointOfSale alloc] init];
+  sale.catalog = catalog;
+  sale.display = display;
+  sale.cart = [Cart cartWithPrices:@[]];
+  [sale createOperations];
+  return sale;
 }
 
++ (PointOfSale *)pointOfSaleWithCatalog:(id <Catalog>)catalog andDisplay:(id <Display>)display andCart:(Cart *)cart {
+  PointOfSale *sale = [[PointOfSale alloc] init];
+  sale.catalog = catalog;
+  sale.display = display;
+  sale.cart = cart;
+  [sale createOperations];
+  return sale;
+}
+
+-(void) createOperations {
+  self.operations = @{
+      FinishCommand.class: [FinishOperation operationWithCart: self.cart andDisplay: self.display],
+      BarcodeScannedCommand.class: [BarcodeScannedOperation operationWithCatalog:self.catalog cart: self.cart andDisplay:self.display]
+  };
+}
+
+#pragma mark - Controller
+
 - (void)execute:(Command *)command {
-  if([command isKindOfClass:[FinishCommand class]]) {
-    [self executeFinishCommand:(FinishCommand *) command];
-  }
-  else if([command isKindOfClass:[BarcodeScannedCommand class]]) {
-    [self executeScannerCommand:(BarcodeScannedCommand *) command];
-  }
+  [self.operations[command.class] run: command];
+
 }
 
 @end
