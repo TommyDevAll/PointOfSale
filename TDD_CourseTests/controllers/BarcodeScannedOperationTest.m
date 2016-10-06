0f4aa6c8623f474a8a66b272d0369a305b0d0207
Tommaso Resti <tommaso.resti@lastminute.com>
2016-10-06 10:59:47 +0200

    BarcodeScannedOperationTest

---
+++ TDD_CourseTests/controllers/BarcodeScannedOperationTest.m
@@ -0,0 +1,61 @@
+//
+//  BarcodeScannedOperationTest.m
+//  TDD_Course
+//
+//  Created by Tommaso Resti on 06/10/16.
+//  Copyright (c) 2016 Tommaso Resti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Display.h"
+#import "Catalog.h"
+#import "OCMock.h"
+#import "BarcodeScannedOperation.h"
+#import "Cart.h"
+#import "BarcodeScannedCommand.h"
+
+
+@interface BarcodeScannedOperationTest : XCTestCase
+@property id<Catalog> catalog;
+@property id<Display> display;
+@end
+
+@implementation BarcodeScannedOperationTest {
+  NSString *A_BARCODE;
+  NSString *NOT_EXISTENT_BARCODE;
+  Price *A_PRICE;
+}
+
+- (void)setUp {
+  [super setUp];
+  A_BARCODE = @"A_BARCODE";
+  NOT_EXISTENT_BARCODE = @"NOT_EXISTENT_BARCODE";
+  A_PRICE = [[Price alloc] init];
+
+  self.catalog = OCMProtocolMock(@protocol(Catalog));
+  self.display = OCMProtocolMock(@protocol(Display));
+}
+
+- (void)test_given_a_valid_barcode {
+  Cart *cart = [Cart new];
+  BarcodeScannedOperation *operation = [BarcodeScannedOperation operationWithCatalog:_catalog cart:cart andDisplay:_display];
+  BarcodeScannedCommand *command = [BarcodeScannedCommand barcodeScannedWithBarcode:A_BARCODE];
+
+  [OCMStub([_catalog findPrice:A_BARCODE]) andReturn:A_PRICE];
+  [operation run:command];
+
+  OCMVerify([_display displayPriceFound:A_PRICE];);
+}
+
+- (void)test_given_a_not_existent_barcode {
+  Cart *cart = [Cart new];
+  BarcodeScannedOperation *operation = [BarcodeScannedOperation operationWithCatalog:_catalog cart:cart andDisplay:_display];
+  BarcodeScannedCommand *command = [BarcodeScannedCommand barcodeScannedWithBarcode:NOT_EXISTENT_BARCODE];
+
+  [OCMStub([_catalog findPrice:NOT_EXISTENT_BARCODE]) andReturn:nil];
+  [operation run:command];
+
+  OCMVerify([_display displayPriceNotFound];);
+}
+
+@end
